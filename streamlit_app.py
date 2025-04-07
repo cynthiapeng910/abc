9539985a96431a194c09425574344f7d87174991
cynthiapeng910 <cynthiapeng910@gmail.com>
2025-04-07 16:03:18 +0800

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,71 @@
+import warnings
+import streamlit as st
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
+
+# Must be the first Streamlit command
+st.set_page_config(page_title="siyan's site", layout="wide") #layout理应有两种，一种PC端一种移动端，这里只做PC端
+
+# Import pages from the new directory
+from page_content.home import home_page
+from page_content.experience import experience_page
+from page_content.education import education_page
+from page_content.resume import resume_page
+from page_content.contact import contact_page
+
+# Import components
+from components.footer import display_footer
+from components.styles import load_css
+
+class MultiApp:
+    def __init__(self):
+        self.apps = []
+
+    def add_app(self, title, func):
+        self.apps.append({"title": title, "function": func})
+
+    def run(self):
+        # Load custom CSS
+        load_css()
+
+        st.sidebar.markdown("## Main Menu")
+        app = st.sidebar.radio(
+            "Navigation", self.apps, format_func=lambda app: app["title"]
+        )
+        st.sidebar.markdown("---")
+
+        app["function"]()
+        
+        # Display footer at the bottom of each page
+        display_footer()
+
+# Initialize the app
+app = MultiApp()
+
+# Add pages to the app
+app.add_app("Home Page", home_page)
+app.add_app("Education Page", education_page)
+app.add_app("Experience Page", experience_page)
+app.add_app("Resume Page", resume_page)
+app.add_app("Contact Page", contact_page)
+
+# Run the app
+app.run()
+
+st.set_page_config(
+    page_title="Your Title",
+    page_icon="🎈",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
+st.markdown("""
+    <style>
+    .css-1d391kg {
+        font-family: 'sans serif';  # 修改这里：将 'sans-serif' 改为 'sans serif'
+    }
+    </style>
+""", unsafe_allow_html=True)
